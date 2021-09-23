17c9e33ba630269d0116fe1efe67874ffa6f4f25
Joao Paulo Diniz <jpaulodiniz@gmail.com>
2021-09-23 16:51:30 -0300

    Organizing JavaDoc

--- src/br/ufmg/labsoft/mutvariants/listeners/ListenerCallsInserter.java
+++ src/br/ufmg/labsoft/mutvariants/listeners/ListenerCallsInserter.java
@@ -1,7 +1,5 @@
 package br.ufmg.labsoft.mutvariants.listeners;
 
-import br.ufmg.labsoft.mutvariants.core.MutantsGenerator;
-
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
@@ -12,26 +10,26 @@ import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.ExplicitConstructorInvocationStmt;
 import com.github.javaparser.ast.stmt.Statement;
 
+import br.ufmg.labsoft.mutvariants.core.MutantsGenerator;
+
 /**
  * Issue #5
  */
 public class ListenerCallsInserter {
 
 	/**
-	 * @param stmt
+	 * @param stmtBody
 	 * @param mGen
-	 * @param mutCountBefore
-	 * @param mutCountAfter
 	 */
 	public static void insertListenerCallInLoopBody(Statement stmtBody, MutantsGenerator mGen) {
 
 		if (stmtBody instanceof BlockStmt) {
-			insertListenerCallInBlockStatement((BlockStmt)stmtBody, mGen);
+			insertListenerCallInBlockStatement((BlockStmt)stmtBody, mGen, true);
 		}
 		else { //loop with just one statement, without {}
 			BlockStmt newBlock = new BlockStmt(new NodeList<Statement>(stmtBody.clone()));
-			insertListenerCallInBlockStatement(newBlock, mGen);
-			stmtBody.replace(newBlock);
+			stmtBody.replace(newBlock); //keep replacing before inserting listener call
+			insertListenerCallInBlockStatement(newBlock, mGen, true);
 		}
 	}
 
@@ -57,11 +55,11 @@ public class ListenerCallsInserter {
 	 */
 	public static void insertListenerCallInConstructorBody(ConstructorDeclaration constructorDecl,
 			MutantsGenerator mGen, long mutCountBefore, long mutCountAfter) {
-		
+
 		if (mutCountAfter > mutCountBefore) {
-			
+
 			BlockStmt constrBody = constructorDecl.getBody();
-			
+
 			if (constrBody.getStatement(0) instanceof ExplicitConstructorInvocationStmt) {
 				insertListenerCallInBlockStatement(constructorDecl.getBody(), 1, mGen);
 			}
@@ -71,27 +69,44 @@ public class ListenerCallsInserter {
 		}
 	}
 
-	/**
-	 * insert <code>listener.listen('methodID')</code> as the first statement
-	 * @param blockStmt
-	 * @param mGen
-	 */
-	private static void insertListenerCallInBlockStatement(BlockStmt blockStmt, MutantsGenerator mGen) {
+	private static void insertListenerCallInBlockStatement(BlockStmt blockStmt,
+			MutantsGenerator mGen) {
 
-		insertListenerCallInBlockStatement(blockStmt, 0, mGen);
+		insertListenerCallInBlockStatement(blockStmt, 0, mGen, false);
+	}
+
+	private static void insertListenerCallInBlockStatement(BlockStmt blockStmt,
+			int position, MutantsGenerator mGen) {
+
+		insertListenerCallInBlockStatement(blockStmt, position, mGen, false);
+	}
+
+	private static void insertListenerCallInBlockStatement(BlockStmt blockStmt,
+			MutantsGenerator mGen, boolean identifyBlockLine) {
+
+		insertListenerCallInBlockStatement(blockStmt, 0, mGen, identifyBlockLine);
 	}
 
 	/**
-	 * insert <code>listener.listen('methodID')</code> as the first statement
+	 * insert <code>listener.listen('methodID[stmtLine]')</code> as the first statement
 	 * @param blockStmt
+	 * @param position
 	 * @param mGen
+	 * @param identifyParentLine useful for different loops in the same method
 	 */
-	private static void insertListenerCallInBlockStatement(BlockStmt blockStmt, int position, MutantsGenerator mGen) {
+	private static void insertListenerCallInBlockStatement(BlockStmt blockStmt,
+			int position, MutantsGenerator mGen, boolean identifyParentLine) {
+
+		String listenerId = mGen.currentClassFQN + "." + mGen.currentOperation;
+		if (identifyParentLine) {
+			int parentStmtLine = blockStmt.getParentNode().get().getBegin().get().line;
+			listenerId += "_" + parentStmtLine;
+		}
 
 		//TODO dinamically retrieve the listener object instantiated in this package
-		MethodCallExpr listenerCall = new MethodCallExpr(new NameExpr("ListenerUtil.listener"), 
-				"listen", 
-				NodeList.nodeList(new StringLiteralExpr(mGen.currentClassFQN + "." + mGen.currentOperation)));
+		MethodCallExpr listenerCall = new MethodCallExpr(new NameExpr("ListenerUtil.listener"),
+				"listen",
+				NodeList.nodeList(new StringLiteralExpr(listenerId)));
 
 		blockStmt.addStatement(position, listenerCall);
 	}
